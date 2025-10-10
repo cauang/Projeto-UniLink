3cba78a3203d029c1c8ab5287b85cd287838cca9
Cauan Gomes <cauangomes.b@gmail.com>
2025-10-10 09:27:58 -0300

    tentando fazer alinhamento da pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ export default function Home() {
             <div className="text-white relative">
               {/* círculo decorativo */}
               <div className="absolute -left-6 -top-6 h-35 w-35 rounded-full border border-white/10" />
+              <div className="relative -top-25 -right-20">
               <h1 className="text-4xl md:text-5xl font-bold leading-tight">
                 Seja um<br/>Voluntário
               </h1>
@@ -19,17 +20,19 @@ export default function Home() {
                 Cadastre-se no Unilink e ajude colegas de diferentes áreas enquanto ganha experiência valiosa.
                 Faça a diferença agora mesmo!
               </p>
-              
               <div className="mt-6">
                 <Link to="/login" className="btn-white">PARTICIPAR</Link>
               </div>
+              </div>
+
               <section className="absolute -bottom-28.5 left-20 w-full">
               <div className="container-page">
                 <div className="relative">
-                  <div className="md:absolute md:-top-10 md:left-8">
-                    <div className="bg-[#6ee7b7] text-[#083344] w-448px h-193px shadow-lg px-6 py-5 border rounded-md border-emerald-200">
-                      <p className="font-semibold fort-sans text-lg">
-                        Conecte-se com alunos da Unifor e contribua para atividades práticas!
+                  <div className="md:absolute md:-top-35">
+                    <div className="bg-[#6ee7b7] text-[#083344] w-[448px] h-[193px] shadow-lg border border-emerald-200 rounded-md
+                                    flex items-center justify-center text-center px-8">
+                      <p className="font-normal text-lg leading-snug whitespace-pre-line">
+                        {"Conecte-se com\n alunos da Unifor e\n contribua para \natividades práticas!"}
                       </p>
                     </div>
                   </div>
@@ -39,14 +42,14 @@ export default function Home() {
             </div>
           
             {/* Imagem à direita com círculos */}
-            <div className="relative">
+            <div className="relative z-[2]">
               <img
                 src="/hero.png"
                 alt="Mãos conectando linhas coloridas"
-                className="w-515px w-671px rounded-md mx-auto"
+                className="w-[515px] h-[671px] rounded-md mx-auto"
               />
               {/* círculo decorativo à direita */}
-              <div className="hidden md:block absolute -right-8 top-10 h-40 w-40 rounded-full border-2 border-white/10" />
+              <div className="hidden md:block absolute -right-35 top-1 h-[253px] w-[253px] rounded-full border-6 border-white/10 z-[1]" />
             </div>
 
           </div>
