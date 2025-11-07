8e03e4561cb7d488a04b727b07b0942ff2778c07
Isadora Neves Rios <isadoraneves.dev@gmail.com>
2025-11-06 21:33:15 -0300

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
                 Faça a diferença agora mesmo!"
               </p>
               <div className="mt-6">
-                <Link to="/sobre" className="btn-white">PARTICIPAR</Link>
+                <Link to="/sobre" className="btn-white">Entrar</Link>
               </div>
               </div>
 
